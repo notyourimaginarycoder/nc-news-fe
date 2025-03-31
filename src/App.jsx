7cce899d74669617108fa5b0ea26083f78b05d28
imaginaryfriend <194876759+notyourimaginarycoder@users.noreply.github.com>
2025-03-31 11:11:21 +0100

    auto commit: Mon 31 Mar 11:11:21 BST 2025

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const cors = require('cors')
+
+app.use(cors())
+
 function App() {
   const [count, setCount] = useState(0)
 
